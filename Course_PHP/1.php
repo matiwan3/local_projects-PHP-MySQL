99ec1cfadc9649fc31d67bc16346aa78a6d136e1
Mateusz <wanmatedzlewicz@gmail.com>
2022-09-26 21:56:28 +0200

    added 26.9

---
+++ Course_PHP/1.php
@@ -0,0 +1,50 @@
+<!-- php -S localhost:4000 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP COURSE</title>
+    
+</head>
+<body>
+    <!-- http://localhost:4000/Desktop/projekty/sql%20php/Course_PHP/1.php -->
+    <?php 
+        // 1
+        // echo("<h1>Mateusz W</h1>");
+        // echo("<hr>");
+        // echo("<p>This is my site</p>");
+
+        // 2 Variables
+        // $characterName = 'Tom';
+        // $characterAge =  80;
+
+        // echo "<h1>Simple Story</h1>";
+        // echo "There once was a man named $characterName <br>";
+        // echo "He was $characterAge years old <br>";
+        // // $characterName = 'Mike';
+        // echo "He really liked the name $characterName <br>";
+        // echo "But didn't like being $characterAge <br>";
+        
+        // 3 Data types
+        // $phrase = "To be or not to be";
+        // $age = 30;
+        // $gpa = 32.54363;
+        // $isMale = true;
+        // echo $phrase;
+
+        // 4 Working with strings
+        $phrase = "Giraffe Academy";
+        // echo strlen($phrase);
+        echo $phrase[0];
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
