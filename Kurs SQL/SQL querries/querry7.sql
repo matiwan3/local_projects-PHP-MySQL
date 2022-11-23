eda10b6f4477e03fb4e6b3fa96b9e2d89c3bc262
Mateusz <wanmatedzlewicz@gmail.com>
2022-11-23 15:47:44 +0100

    Update querry7.sql

--- Kurs SQL/SQL querries/querry7.sql
+++ Kurs SQL/SQL querries/querry7.sql
@@ -1,3 +1,4 @@
-SELECT * FROM produkty WHERE id_produktu = 18;
+SELECT * FROM produkty WHERE id_produktu IN(19,20);
 
 UPDATE produkty SET model = 'EP-350X', cena = 3200 WHERE id_produktu = 18;
+UPDATE produkty SET cena = cena - 100 WHERE id_produktu IN(19,20);
