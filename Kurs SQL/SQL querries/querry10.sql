d7b7ff19688a8104dfeec4f6604a3069e03b6637
Mateusz <wanmatedzlewicz@gmail.com>
2022-12-15 21:24:39 +0100

    Update querry10.sql

--- Kurs SQL/SQL querries/querry10.sql
+++ Kurs SQL/SQL querries/querry10.sql
@@ -3,6 +3,6 @@ SELECT model, cena,
 CASE 
 	WHEN cena <= 2000 THEN 'TANIO'
     WHEN cena >= 3000 THEN 'DROGO'
-    ELSE 'ŚREDNIO'
+    ELSE 'ŚREDNIO' # / cena
 END AS cena_własna
-FOM produkty ORDER BY cena_własna DESC;
+FROM produkty ORDER BY cena_własna DESC;
